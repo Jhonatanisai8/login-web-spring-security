0d6d48248513fa173bf97fb451477a970b55b11b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-25 09:22:39 -0500

    controlador para el registro de usuarios

---
+++ src/main/java/com/isai/demologinspringboot/app/controllers/UserRegisterController.java
@@ -0,0 +1,36 @@
+package com.isai.demologinspringboot.app.controllers;
+
+import com.isai.demologinspringboot.app.dtos.UserRequest;
+import com.isai.demologinspringboot.app.services.impl.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/register")
+@RequiredArgsConstructor
+public class UserRegisterController {
+
+    private final UserServiceImpl userService;
+
+    @ModelAttribute("user")
+    public UserRequest newUserRequest() {
+        return new UserRequest();
+    }
+
+    @GetMapping
+    public String showRegisterForm(Model model) {
+        return "register";
+    }
+
+    @PostMapping
+    public String processRegisterForm(@ModelAttribute("user") UserRequest userRequest, Model model) {
+        userService.saveUser(userRequest);
+        return "redirect:/register?exito";
+    }
+
+}
