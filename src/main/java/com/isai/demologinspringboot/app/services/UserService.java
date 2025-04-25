0f09cfd0540f8983a9ccf1bf8b1458e4fe8466aa
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-25 09:16:53 -0500

    service para la entidad user

---
+++ src/main/java/com/isai/demologinspringboot/app/services/UserService.java
@@ -0,0 +1,6 @@
+package com.isai.demologinspringboot.app.services;
+
+public interface UserService
+        <T, W> {
+    T saveUser(W entity);
+}
---
+++ src/main/java/com/isai/demologinspringboot/app/services/impl/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.isai.demologinspringboot.app.services.impl;
+
+import com.isai.demologinspringboot.app.dtos.UserRequest;
+import com.isai.demologinspringboot.app.models.Rol;
+import com.isai.demologinspringboot.app.models.UserEntity;
+import com.isai.demologinspringboot.app.repositorys.UserRepository;
+import com.isai.demologinspringboot.app.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl
+        implements UserService<UserEntity, UserRequest> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserEntity saveUser(UserRequest entity) {
+        return userRepository.save(convertUserRequestToUserEntity(entity));
+    }
+
+    private UserEntity convertUserRequestToUserEntity(UserRequest userRequest) {
+        Rol rol = new Rol();
+        rol.setNameRol("ROLE_USER");
+        UserEntity userEntity = new UserEntity();
+        userEntity.setFirstNameUser(userRequest.getFirstNameUser());
+        userEntity.setLastNameUser(userRequest.getLastNameUser());
+        userEntity.setEmailUser(userRequest.getEmailUser());
+        userEntity.setPasswordUser(userRequest.getPasswordUser());
+        userEntity.setRoles(Set.of(rol));
+        return userEntity;
+    }
+}
