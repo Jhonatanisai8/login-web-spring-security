197ed248bbc6e9425b314b97ece06695e61af2e2
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-28 10:23:44 -0500

    controlador y vista para el login

---
+++ src/main/java/com/isai/demologinspringboot/app/controllers/RegistryController.java
@@ -0,0 +1,15 @@
+package com.isai.demologinspringboot.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegistryController {
+
+    @GetMapping(path = "/login")
+    public String initSession(Model model) {
+        model.addAttribute("title", "Login");
+        return "login";
+    }
+}
