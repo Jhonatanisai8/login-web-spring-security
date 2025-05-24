96c0c9d3a9a02226a10f23d6a4d299331720105d
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-24 10:27:16 -0500

    controlador para tipo-membresia para la vista de mostrar las membresias

--- src/main/java/com/isai/demologinspringboot/app/controllers/MembershipsController.java
+++ src/main/java/com/isai/demologinspringboot/app/controllers/MembershipsController.java
@@ -1,5 +1,12 @@
 package com.isai.demologinspringboot.app.controllers;
 
+import com.isai.demologinspringboot.app.models.MembershipType;
+import com.isai.demologinspringboot.app.services.impl.MembershipTypeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +14,19 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(path = "/admin")
+@RequiredArgsConstructor
 public class MembershipsController {
 
-    @GetMapping("/memberships")
-    public ModelAndView showCoaches() {
-        return new ModelAndView("admin/memberships/memberships");
+    private final MembershipTypeService membershipTypeService;
+
+    @GetMapping(path = "/memberships")
+    public ModelAndView listMoviesHome(
+            @PageableDefault(sort = "price", direction = Sort.Direction.DESC)
+            Pageable pageable) {
+        Page<MembershipType> membershipTypesPage = membershipTypeService.findAllMembershipTypes(pageable);
+        return new ModelAndView("admin/memberships/show")
+                .addObject("membershipTypesPage", membershipTypesPage);
     }
+
 
 }
