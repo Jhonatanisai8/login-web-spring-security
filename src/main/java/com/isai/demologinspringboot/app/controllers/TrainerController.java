ecec12d8bf670bfdb8206babede8f96e0dad5c5f
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-24 01:46:48 -0500

    TrainerController: implementacion para que se listen las especiales en el formulario de crear entrenador

--- src/main/java/com/isai/demologinspringboot/app/controllers/TrainerController.java
+++ src/main/java/com/isai/demologinspringboot/app/controllers/TrainerController.java
@@ -1,6 +1,8 @@
 package com.isai.demologinspringboot.app.controllers;
 
+import com.isai.demologinspringboot.app.models.Speciality;
 import com.isai.demologinspringboot.app.models.Trainer;
+import com.isai.demologinspringboot.app.services.impl.SpecialityService;
 import com.isai.demologinspringboot.app.services.impl.TrainerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(path = "/admin")
 @RequiredArgsConstructor
@@ -19,6 +23,8 @@ public class TrainerController {
 
     private final TrainerService trainerService;
 
+    private final SpecialityService specialityService;
+
     @GetMapping("/trainers")
     public ModelAndView showCoaches(@PageableDefault(sort = "firstName", size = 5) Pageable pageable) {
         Page<Trainer> trainersPage = trainerService.findAllTrainers(pageable);
@@ -26,4 +32,11 @@ public class TrainerController {
                 "trainersPage", trainersPage);
     }
 
+    @GetMapping("/trainers/new")
+    public ModelAndView showFormNewTrainer() {
+        List<Speciality> specialitiesBD = specialityService.findAllSpecialities();
+        return new ModelAndView("admin/trainers/create-trainer",
+                "trainer", new Trainer())
+                .addObject("specialitiesBD", specialitiesBD);
+    }
 }
