3cf3339cc392cfa9d58cce8a7bedc938665994f5
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-28 01:15:40 -0500

    agregando depencias para ollama (I-A)

--- src/main/java/com/isai/demologinspringboot/app/controllers/AdminController.java
+++ src/main/java/com/isai/demologinspringboot/app/controllers/AdminController.java
@@ -2,7 +2,8 @@ package com.isai.demologinspringboot.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//docker compose -f docker-compose.yml up
+//docker exec -it ollama ollama run llama2
 @Controller
 @RequestMapping(path = "/admin")
 public class AdminController {
